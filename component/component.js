f42500f9b06065fd4452e66d61112a251518e869
terada-m <terada-m@iij.ad.jp>
2017-08-08 18:46:46 +0900

    finish implement.

--- component/component.js
+++ component/component.js
@@ -3,38 +3,72 @@
 define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
 
   exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
-    driverName: '%%DRIVERNAME%%',
     /* ^--- And here */
 
     // Write your component here, starting with setting 'model' to a machine with your config populated
+
+    driverName: '%%DRIVERNAME%%',
+
+    //state manage variables of static data.
+    gotStaticData: false,
+
+    //assign method
+    systemStorageAssign: 'new',
+    dataStorageAssign: 'new',
+    //view control.
+    systemStorageDisable: '',
+    ibaDisable: 'disabled',
+    restoreImageDisable: 'disabled',
+    ibbDisable: 'disabled',
+    dataStorageDisable: '',
+
+    willRender() {
+
+      if (!this.get('gotStaticData')) {
+
+        let url = 'https://storage-dag.iijgio.com/iij-rancher-ui/static.json';
+        let proxyUrl = this.get('app.proxyEndpoint') + '/' + url;
+        console.debug('proxyUrl:' + proxyUrl);
+
+        $.getJSON(proxyUrl).then(data => {
+          this.set('model.static', data);
+        });
+
+        this.set('gotStaticData', true)
+      }
+    },
+
     bootstrap: function () {
       let config = this.get('store').createRecord({
         type: '%%DRIVERNAME%%Config',
-        gisServiceCode: '',
+        //Account
+        gis: '',
         accessKey: '',
         secretKey: '',
-        ivmServiceCode: '',
-        ibaServiceCode: '',
-        iPv4: '',
-        iPv6: '',
-        dockerPort: 2376,
-        serverType: 'VB0-1',
-        imageName: 'S30GB_CENTOS7_64',
-        storageGroup: '',
+
+        //Instance
+        serverType: '',
         serverGroup: '',
-        ibbServiceCode: '',
-        addStorageType: '',
+        ivm: '', //dose not set value.
+
+        //storage group
+        storageGroup: '',
+
+        //System Dtorage
+        imageName: '',//NEW SYSTEM Image
+        iba: '', //Attach Already existing storage
+        restoreId: '',//Restore
+
+        //Data Storage
+        ibb: '',
+        dataStorage: '',
+
+        //Network
         privateMode: 'false',
+        dockerPort: 2376,
       });
 
-      // let view_state = this.get('store').createRecord(
-      //   {
-      //   type: 'view_state',
-      //   iba_disable: '',
-      // });
-
       let type = 'host';
-
       if (!this.get('useHost')) {
         type = 'machine';
       }
@@ -44,34 +78,92 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
         '%%DRIVERNAME%%Config': config,
       }));
     },
-    getStatic() {
-      let url = 'https://storage-dag.iijgio.com/iij-rancher-ui/static.json';
-
-      let proxyUrl = this.get('app.proxyEndpoint') + '/' + url;
-
-      console.debug('proxyUrl:' + proxyUrl);
-      return Ember.$.ajax(
-        {
-          url: proxyUrl,
-          method: 'GET',
-          dataType: 'json'
-        }).done(function (data) {
-          return data;
-        }
-      );
-    },
-    staticData: this.getStatic,
-    system_storage_disable: '',
-    iba_disable: 'disabled',
-    restore_image_disable: 'disabled',
-
     // Add custom validation beyond what can be done from the config API schema
     validate() {
       // Get generic API validation errors
       this._super();
       var errors = this.get('errors') || [];
 
+      //for debug
+      console.debug('gis=' + this.get('model.%%DRIVERNAME%%Config.gis'));
+      console.debug('accessKey=' + this.get('model.%%DRIVERNAME%%Config.accessKey'));
+      console.debug('secretKey=' + this.get('model.%%DRIVERNAME%%Config.secretKey'));
+      console.debug('serverType=' + this.get('model.%%DRIVERNAME%%Config.serverType'));
+      console.debug('serverGroup=' + this.get('model.%%DRIVERNAME%%Config.serverGroup'));
+      console.debug('ivm=' + this.get('model.%%DRIVERNAME%%Config.ivm'));
+      console.debug('storageGroup=' + this.get('model.%%DRIVERNAME%%Config.storageGroup'));
+      console.debug('imageName=' + this.get('model.%%DRIVERNAME%%Config.imageName'));
+      console.debug('iba=' + this.get('model.%%DRIVERNAME%%Config.iba'));
+      console.debug('restoreId=' + this.get('model.%%DRIVERNAME%%Config.restoreId'));
+      console.debug('ibb=' + this.get('model.%%DRIVERNAME%%Config.ibb'));
+      console.debug('dataStorage=' + this.get('model.%%DRIVERNAME%%Config.dataStorage'));
+      console.debug('privateMode=' + this.get('model.%%DRIVERNAME%%Config.privateMode'));
+      console.debug('iba=' + this.get('model.%%DRIVERNAME%%Config.iba'));
+      console.debug('dockerPort=' + this.get('model.%%DRIVERNAME%%Config.dockerPort'));
+
       // Add more specific errors
+
+      // mandatory
+      if (!this.get('model.%%DRIVERNAME%%Config.gis')) {
+        errors.push('Service Code is required');
+      }
+
+      if (!this.get('model.%%DRIVERNAME%%Config.accessKey')) {
+        errors.push('Access Key ID is required');
+      }
+
+      if (!this.get('model.%%DRIVERNAME%%Config.secretKey')) {
+        errors.push('Secret Key is required');
+      }
+
+      var systemStorageAssignMethod = this.get('systemStorageAssign');
+
+      switch (systemStorageAssignMethod) {
+        case 'new' :
+          this.set('model.%%DRIVERNAME%%Config.iba', '');
+          this.set('model.%%DRIVERNAME%%Config.restoreId', '');
+          break;
+        case 'restore' :
+          if (!this.get('model.%%DRIVERNAME%%Config.restoreId')) {
+            errors.push('Storage Archive ID is required');
+            break;
+          }
+          this.set('model.%%DRIVERNAME%%Config.iba', '');
+          this.set('model.%%DRIVERNAME%%Config.imageName', '');
+          this.$('#systemStorageSB').each(function () {
+            this.selectedIndex = 0;
+          });
+          break;
+        case 'assign' :
+          if (!this.get('model.%%DRIVERNAME%%Config.iba')) {
+            errors.push('System Storage Service Code is required');
+            break;
+          }
+          this.set('model.%%DRIVERNAME%%Config.imageName', '');
+          this.set('model.%%DRIVERNAME%%Config.restoreId', '');
+          this.$('#systemStorageSB').each(function () {
+            this.selectedIndex = 0;
+          });
+          break;
+      }
+
+      var dataStorageAssignMethod = this.get('dataStorageAssign');
+
+      switch (dataStorageAssignMethod) {
+        case 'new' :
+          this.set('model.%%DRIVERNAME%%Config.ibb', '');
+          break;
+        case 'assign' :
+          if (!this.get('model.%%DRIVERNAME%%Config.ibb')) {
+            errors.push('Data Storage Type is required');
+            break;
+          }
+          this.set('model.%%DRIVERNAME%%Config.imageName', '');
+          this.$('#dataStorageSB').each(function () {
+            this.selectedIndex = 0;
+          });
+          break;
+      }
 
       // Check something and add an error entry if it fails:
       if (parseInt(this.get('model.%%DRIVERNAME%%Config.size'), 10) < 1024) {
@@ -90,26 +182,61 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
       }
     },
     actions: {
-      storage_assign_method: function (value) {
+      selectStorageAttach: function (value) {
         switch (value) {
           case 'new':
-            this.set('system_storage_disable', '');
-            this.set('iba_disable', 'disabled');
-            this.set('restore_image_disable', 'disabled');
+            this.set('systemStorageAssign', value);
+            this.set('systemStorageDisable', '');
+            this.set('ibaDisable', 'disabled');
+            this.set('restoreImageDisable', 'disabled');
             break;
           case 'restore':
-            this.set('system_storage_disable', 'disabled');
-            this.set('iba_disable', 'disabled');
-            this.set('restore_image_disable', '');
+            this.set('systemStorageAssign', value);
+            this.set('systemStorageDisable', 'disabled');
+            this.set('ibaDisable', 'disabled');
+            this.set('restoreImageDisable', '');
             break;
           case 'assign':
-            this.set('system_storage_disable', 'disabled');
-            this.set('iba_disable', '');
-            this.set('restore_image_disable', 'disabled');
+            this.set('systemStorageAssign', value);
+            this.set('systemStorageDisable', 'disabled');
+            this.set('ibaDisable', '');
+            this.set('restoreImageDisable', 'disabled');
             break;
         }
-
-        console.debug('fired!' + value + this.getStatic());
+      },
+      selectDataStorageAttach: function (value) {
+        switch (value) {
+          case 'new':
+            this.set('dataStorageAssign', value);
+            this.set('dataStorageDisable', '');
+            this.set('ibbDisable', 'disabled');
+            break;
+          case 'assign':
+            this.set('dataStorageAssign', value);
+            this.set('dataStorageDisable', 'disabled');
+            this.set('ibbDisable', '');
+            break;
+        }
+      },
+      selectImageType: function (value) {
+        console.debug('imageType=' + value);
+        this.set('model.%%DRIVERNAME%%Config.imageName', value)
+      },
+      selectServerType: function (value) {
+        console.debug('serverType=' + value);
+        this.set('model.%%DRIVERNAME%%Config.serverType', value)
+      },
+      selectServerGroup: function (value) {
+        //console.debug('serverGroup=' + value);
+        this.set('model.%%DRIVERNAME%%Config.serverGroup', value)
+      },
+      selectStorageGroup: function (value) {
+        console.debug('storageGroup=' + value);
+        this.set('model.%%DRIVERNAME%%Config.storageGroup', value)
+      },
+      selectDataStorageType: function (value) {
+        console.debug('dataStorageType=' + value);
+        this.set('model.%%DRIVERNAME%%Config.dataStorage', value)
       }
     },
   });
